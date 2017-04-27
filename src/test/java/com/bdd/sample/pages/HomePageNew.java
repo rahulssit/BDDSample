e8a81d059f887ff6472d5f4443c598c8ba3bc8e2
Bhaskar Hariharan <Bhaskar Hariharan>
2017-04-27 11:07:55 -0500

    changing the folder name for fixing report generation

--- src/test/java/com/bdd/sample/pages/HomePageNew.java
+++ src/test/java/com/bdd/sample/pages/HomePageNew.java
@@ -4,8 +4,10 @@ import ch.lambdaj.function.convert.Converter;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.server.handler.FindElement;
 
 import net.thucydides.core.pages.PageObject;
 
@@ -19,7 +21,10 @@ public class HomePageNew extends PageObject {
 	@FindBy(id="search-query")
     private WebElementFacade searchText;
 	
-	@FindBy(xpath="//*[@id='search-bar']/div/div[2]/button")
+	/*@FindBy(xpath="//*[@id='gnav-search']/div/div[2]/button") 
+    private WebElementFacade searchbutton;*/
+	
+	@FindBy(xpath="//*[@id='search-bar']/div/div[2]/button") 
     private WebElementFacade searchbutton;
 	
 	@FindBy(xpath="//*[@id='content']/div/div/div[2]/div/h4")
@@ -34,7 +39,6 @@ public class HomePageNew extends PageObject {
     public void verify_shoes_product_page() {
     	shoelandingpage.isPresent();
     }
-    
     
     
     public void search_product(String keyword) {
--- src/test/java/com/bdd/sample/steps/serenity/EndUserStepsNew.java
+++ src/test/java/com/bdd/sample/steps/serenity/EndUserStepsNew.java
@@ -1,13 +1,11 @@
 package com.bdd.sample.steps.serenity;
 
-import com.bdd.sample.pages.DictionaryPage;
-import com.bdd.sample.pages.HomePageNew;
-
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
+import com.bdd.sample.pages.HomePageNew;
 
 public class EndUserStepsNew extends ScenarioSteps {
 
